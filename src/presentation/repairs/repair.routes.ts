0cdf59be8d7321c97bfbf717726dafd578865517
pabloss22222 <pabloss22222@gmail.com>
2024-07-27 05:24:42 -0500

    proy_v5

--- src/presentation/repairs/repair.routes.ts
+++ src/presentation/repairs/repair.routes.ts
@@ -14,7 +14,7 @@ export class RepairRoutes {
         const repairController = new RepairController(repairService);
 
         router.post('/', repairController.createRepair);
-        router.use(AuthMiddleware.protect, AuthMiddleware.restrictTo)
+        router.use(AuthMiddleware.protect, AuthMiddleware.restrictTo('EMPLOYEE'))
         router.get('/', repairController.findAllRepairs);
         router.get('/:id', repairController.findRepairById);
         router.patch('/:id', repairController.updateRepairById);
